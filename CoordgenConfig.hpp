cdb09b3d67a0d1d01603458faff66b36c409761f
Ricardo R-S <rodriguez@schrodinger.com>
2021-01-20 12:54:16 -0500

    Reafactor

--- CoordgenConfig.hpp
+++ CoordgenConfig.hpp
@@ -2,25 +2,21 @@
 
 #ifndef STATIC_COORDGEN
 
-#ifdef IN_COORDGEN
 #ifdef WIN32
+#ifdef IN_COORDGEN
 #define EXPORT_COORDGEN __declspec(dllexport)
 #else
-#define EXPORT_COORDGEN __attribute__((visibility("default")))
-#endif
-
-#else
-
-#ifdef WIN32
 #define EXPORT_COORDGEN __declspec(dllimport)
-#else
-#define EXPORT_COORDGEN __attribute__((visibility("default")))
-#endif
+#endif // IN_COORDGEN
 
-#endif
+#else
+
+#define EXPORT_COORDGEN __attribute__((visibility("default")))
+
+#endif // WIN32
 
 #else
 
 #define EXPORT_COORDGEN
 
-#endif
+#endif // STATIC_COORDGEN
